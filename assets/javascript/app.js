62efb072791a6b34540911254a14c39a1e4bcbb4
sally <sallyakrum83@gmail.com>
2019-01-02 16:35:26 -0600

    added some changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,51 +21,48 @@ var unanswered=0;
   time = 0;
   if (!clockRunning) {
     
-    intervalId = setInterval(count,1000);
+    intervalId = setInterval(decrement,1000);
     clockRunning = true;
     console.log(intervalId);
-    $("#display").html("<p>" + "Time Remaining:" + clockRunning +"</p>");
+   
   }
 
 });
 
-function count() {
-  // increment time by 1
-  time++;
-  // Get the current time, pass that into the timeConverter function
-  var converted = timeConverter(time);
-  console.log(converted);
+function decrement() {
+  // decrement time by 1
 
-  // Use the variable we just created to show the converted time in the "display" div.
-  $("#display").text("Time Remaining :" + converted);
+    timeRemaining--;
 
-function timeConverter(t) {
-  var minutes = Math.floor(t / 60);
-  var seconds = t - minutes * 60;
+    $("#display").html("<p>" + "Time Remaining:" + timeRemaining +"</p>");
 
-  if (seconds < 10) {
-    seconds = "0" + seconds;
+    if (timeRemaining === 0) {
+
+      stop();
+
+      recordLap();
+    }
   }
+  function stop() {
 
-  if (minutes === 0) {
-    minutes = "00";
-  } else if (minutes < 10) {
-    minutes = "0" + minutes;
+    clearInterval(intervalId);
   }
-
-  return minutes + ":" + seconds;
-}
-}
 
 // define what happend when click the done button 
 $("#done-button").on("click", function(){
 
   clearInterval(intervalId);
   clockRunning = false;
-   
-    var ques1 =ques1.value;
-    var ques2 =ques2.value;
-    var ques3 =ques3.value;
+  check();
+  recordLap();
+  $("#after-done").text(recordLap);
+  console.log(recordLap);
+});
+
+function check(){
+    var ques1 ="";
+    var ques2 ="";
+    var ques3 ="";
 
      if (ques1=="The lion king"){
        correctAnswer++;
@@ -88,25 +85,22 @@ $("#done-button").on("click", function(){
     if (unanswered){
       unanswered++;
     }
-    
-  recordLap();
-    
-});
+  }
 
 function recordLap() {
      
 //  Add the current lap 
- $("#after-done").append("<h1> " +  "Totally Trivial Trivia !" + "</h1>");
- $("#after-done").append("<h2> " +  "All Done !" + "</h2>");
- $("#after-done").style.visibility ="visible";
+ $("#game-name").html("<h1>Totally Trivial Trivia!</h1>");
+ $("#All-Done").html("<h2>All Done!</h2>");
  $("#number-correct").html("<p>" + "correctAnswer:" + correctAnswer + "</p>");
  $("#number-incorrect").html("<p>" + "incorrectAnswer:" + incorrectAnswer + "</p>");
- $("#number-unanswered").html("<p>" + "unanswered:" + unanswered + "</p>");
+ $("#number-unaswered").html("<p>" + "unanswered:" + unanswered + "</p>");
     // Increment lap by 1. 
     lap++;
     console.log(recordLap);
   }
   if (time === timeRemaining) {
     recordLap();
+    $("#after-done").text(recordLap);
   }
 
